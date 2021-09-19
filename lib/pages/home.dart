7442c4bcd6bb1476cdee0e81e562baa08b28656d
Tenji <patricepetit@hotmail.com>
2021-09-19 11:23:01 +0200

    feature/notification: add notification form

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:poc_cdm/helpers/socket.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 // import 'package:package_info_plus/package_info_plus.dart';
-import 'package:poc_cdm/widget/form.dart';
+import 'package:poc_cdm/widget/socketForm.dart';
+import 'package:poc_cdm/widget/notificationForm.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -15,10 +16,11 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   IO.Socket? socket;
+  BuildContext? homeContext;
 
   @override
   void initState() {
-    print('initState');
+    // print('initState');
     // DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
     // deviceInfo.androidInfo.then((androidInfo) {
     //   print('Running on ${androidInfo.id}'); // e.g. "Moto G (4)"
@@ -31,7 +33,7 @@ class _HomeState extends State<Home> {
 
   @override
   void dispose() {
-    print('dispose');
+    // print('dispose');
 
     if (this.socket!.connected) {
       this.socket!.disconnect();
@@ -39,12 +41,26 @@ class _HomeState extends State<Home> {
     super.dispose();
   }
 
-  onValidate(String host, String port, String namespace) {
-    print("$host $port $namespace");
-    print("${this.socket == null}");
+  onValidateSocket(String host, String port, String namespace) {
+    // print("$host $port $namespace");
+    // print("${this.socket == null}");
     if (this.socket == null) {
-      print('socket creation');
+      // print('socket creation');
       this.socket = socketFactory('http://$host:$port/$namespace', "1.0.0");
+      this.socket!.on('error', (dynamic err) {
+        print("${err}");
+        String message = err!.message;
+        final snackBar = SnackBar(
+          content: Text(message),
+          action: SnackBarAction(
+            label: 'Undo',
+            onPressed: () {
+              // Some code to undo the change.
+            },
+          ),
+        );
+        ScaffoldMessenger.of(this.context).showSnackBar(snackBar);
+      });
       this.socket!.connect();
     } else {
       this.socket!.disconnect();
@@ -53,14 +69,23 @@ class _HomeState extends State<Home> {
     }
   }
 
+  onValidateNotification(String message) {
+    if (this.socket!.connected) {
+      this.socket!.emit('@cdm/notification', message);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    String connect = "Disconnected";
-    print(this.socket != null);
-    print(this.socket?.connected);
-    if (this.socket != null && this.socket!.connected) {
-      connect = "Connected";
-    }
+    // String connect = "Disconnected";
+    // print(this.socket != null);
+    // print(this.socket?.connected);
+    // if (this.socket != null && this.socket!.connected) {
+    //   connect = "Connected";
+    // }
+
+    this.homeContext = context;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('device manager poc'),
@@ -68,10 +93,12 @@ class _HomeState extends State<Home> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Text("coucou"),
             SocketForm(
-              onValidate: this.onValidate,
+              onValidate: this.onValidateSocket,
             ),
+            NotificationForm(
+              onValidate: this.onValidateNotification,
+            )
           ],
         ),
       ),
---
+++ lib/widget/notificationForm.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+class NotificationForm extends StatefulWidget {
+  final Function onValidate;
+
+  const NotificationForm({Key? key, required this.onValidate})
+      : super(key: key);
+
+  @override
+  _NotificationFormState createState() => _NotificationFormState();
+}
+
+class _NotificationFormState extends State<NotificationForm> {
+  final formKey = GlobalKey<FormState>();
+  final TextEditingController fname = TextEditingController();
+  String hostname = "";
+  String port = "";
+  String namespace = "";
+
+  @override
+  Widget build(BuildContext context) {
+    final double height = MediaQuery.of(context).size.height;
+    return SingleChildScrollView(
+      padding: const EdgeInsets.only(left: 40, right: 40),
+      child: Form(
+        key: formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: height * 0.04,
+            ),
+            Text("Enter URL",
+                style: TextStyle(fontSize: 30, color: Color(0xFF363f93))),
+            SizedBox(
+              height: height * 0.04,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Enter the hostname"),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Enter correct hostname";
+                } else {
+                  hostname = value;
+                  return null;
+                }
+              },
+            ),
+            SizedBox(
+              height: height * 0.04,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Enter the port"),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Enter correct port";
+                } else {
+                  port = value;
+                  return null;
+                }
+              },
+            ),
+            SizedBox(
+              height: height * 0.04,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Enter the namespace"),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Enter correct namespace";
+                } else {
+                  namespace = value;
+                  return null;
+                }
+              },
+            ),
+            SizedBox(
+              height: height * 0.04,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xFF363f93),
+                    shape: CircleBorder(),
+                  ),
+                  child: Icon(
+                    Icons.arrow_right,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      this.widget.onValidate(hostname, port, namespace);
+                    }
+                  },
+                ),
+                // NeumorphicButton(
+                //   margin: EdgeInsets.only(top: 12),
+                //   onPressed: () {
+                //     if (formKey.currentState!.validate()) {
+                //       final snackBar =
+                //           SnackBar(content: Text("Submitting form"));
+                //       _scafffoldkey.currentState!.showSnackBar(snackBar);
+                //     }
+                //   },
+                //   style: NeumorphicStyle(
+                //     color: Color(0xFF363f93),
+                //     shape: NeumorphicShape.concave,
+                //     boxShape: NeumorphicBoxShape.circle(),
+                //     border: NeumorphicBorder(
+                //       isEnabled: false,
+                //       color: Color(0x33000000),
+                //       width: 0.8,
+                //     ),
+                //   ),
+                //   child: Icon(Icons.arrow_right, color: Colors.white),
+                // ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
